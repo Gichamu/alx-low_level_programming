91c005f984df333b47f32b7813e7ca8e4d26bf8d
Gichamu <gichamu007@gmail.com>
2025-06-24 14:38:47 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (sucess)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
+}
