1c9aaaed2189e5ff3c2e87d8e34da8a38f7ac3d5
Gichamu <gichamu007@gmail.com>
2025-07-06 02:20:42 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,12 +22,9 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
-	else
+	else if (n % 10 == 0)
 	{
-		while (n % 10 == 0)
-		{
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-		}
 	}
 	printf("\n");
 	return (0);
