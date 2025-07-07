0f72bc0c919aeab0b18f22f82aba807225543f07
Gichamu <gichamu007@gmail.com>
2025-07-07 12:43:31 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	int a = 122;
+
+	while (a >= 97)
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
