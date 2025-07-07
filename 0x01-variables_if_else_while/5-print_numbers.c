74fdf833de6dbefdae4b8359e34cd3a9a59e2f24
Gichamu <gichamu007@gmail.com>
2025-07-07 12:05:30 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
