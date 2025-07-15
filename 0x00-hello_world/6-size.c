b14a6b1cb880c647ff396ffbbc03cbec7e6b346b
Gichamu <gichamu007@gmail.com>
2025-07-15 11:20:09 -0700

    100-intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	sizeof a char;
+	sizeof an int;
+	sizeof a long long int;
+	sizeof a float;
+	return (0);
+}
