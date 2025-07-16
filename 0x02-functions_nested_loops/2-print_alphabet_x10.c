ede1907185060a06b1156aeab913500f1d100ba7
Gichamu <gichamu007@gmail.com>
2025-07-16 10:05:06 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - print the alphabet 10 times in lowercase
+*Return: 0
+*/
+void print_alphabet_x10(void);
+{
+	int count = 0;
+	int a = 97;
+
+	while (count <= 9)
+	{
+		while (a <= 122)
+		{
+			putchar(a);
+			a++;
+		}
+		putchar(count);
+		count++;
+	}
+}
