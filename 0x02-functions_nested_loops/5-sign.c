7281673eee1fa0394d61b80d4cec40ac4ac4606d
Gichamu <gichamu007@gmail.com>
2025-07-17 03:31:17 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,20 +7,20 @@
 */
 int print_sign(int n)
 {
-	if (n >= 49 && n <= 57)
+	if (n > 48)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
-	else if (n == 48)
+	else if (n == 0)
 	{
-		return (0);
 		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
 	}
 	_putchar('\n');
 }
