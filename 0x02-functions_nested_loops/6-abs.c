17e589865f65e440554ca2c14bd2fd51f5f8a9f1
Gichamu <gichamu007@gmail.com>
2025-07-17 04:08:28 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_abs - computes absolute value of an integer
+*@n: integer value it takes as argument
+*Return: 0
+*/
+int _abs(int)
+{
+	if (n > 48)
+	{
+		_putchar(n);
+	}
+	else if (n == 0)
+	{
+		_putchar(n);
+	}
+	else
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
