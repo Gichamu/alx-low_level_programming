f925f949a4373e39188936e605c2a3c9c41b6564
Gichamu <gichamu007@gmail.com>
2025-07-18 12:24:33 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Return:0
+*/
+int main(void)
+{
+	int a, b;
+
+	for (a = 0x30; a <= 0x39; a++)
+	{
+		putchar(a);
+	}
+	for (b = 0x41; b <= 0x46; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
