2b039f19730865132caa1d715f075bc5e7f01580
Gichamu <gichamu007@gmail.com>
2025-07-19 07:16:32 -0700

    repeat

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*add - adds two integers and returns results
+*@a: the first integer
+*@b: the second integer
+*Return: sum of two integers
+*/
+int add(int, int);
+{
+	int a, b;
+	sum = a + b
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*times_table - prints the 9 times table
+*Return: 9 times table
+*/
+void times_table(void)
+{
+	int i, j;
+
+	for
+}
