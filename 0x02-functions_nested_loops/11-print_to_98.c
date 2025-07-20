22fe2356b737a188a127ed998177c13429c8b0bb
Gichamu <gichamu007@gmail.com>
2025-07-20 10:46:57 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*print_to_98 - prints natural numbers to 98
+*
+*Return: natural numbers to 98
+*/
+void print_to_98(int n)
+{
+	for (n = 1; n <= 98; n++)
+	{
+		printf("%d, ", n);
+	}
+}
