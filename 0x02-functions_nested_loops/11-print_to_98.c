3b7db18a2e207da45e9066272ae3bf4caef6a1f0
Gichamu <gichamu007@gmail.com>
2025-07-20 12:45:57 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,12 +3,12 @@
 
 /**
 *print_to_98 - prints natural numbers to 98
-*
+*@n: integer
 *Return: natural numbers to 98
 */
 void print_to_98(int n)
 {
-	while (n < 99)
+	while (n <= 98)
 	{
 	printf("%d, ", n);
 	n++;
@@ -17,7 +17,7 @@ void print_to_98(int n)
 	{
 		printf("%d, ", n);
 	}
-	while (n >98)
+	while (n >= 98)
 	{
 		printf("%d, ", n);
 		n--;
