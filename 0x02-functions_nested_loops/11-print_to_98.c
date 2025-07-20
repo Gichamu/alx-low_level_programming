d52703a2b6aa9f22ca397210cd1227316d5a60d9
Gichamu <gichamu007@gmail.com>
2025-07-20 11:15:56 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,8 +8,19 @@
 */
 void print_to_98(int n)
 {
-	for (n = 1; n <= 98; n++)
+	if (n < 99)
+	{
+	printf("%d, ", n);
+	n++;
+	}
+	else if (n == 98)
 	{
 		printf("%d, ", n);
 	}
+	else
+	{
+		printf("%d, ", n);
+		n--;
+	}
 }
+
