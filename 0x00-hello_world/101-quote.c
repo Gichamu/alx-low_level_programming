f6d2104dac61b4864d2bfeb8e3a5a77d816af244
Gichamu <gichamu007@gmail.com>
2025-07-21 00:52:53 -0700

    all commits

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	char msg[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	write(2, msg, sizeof(msg) -2);
+	return (1);
+}
