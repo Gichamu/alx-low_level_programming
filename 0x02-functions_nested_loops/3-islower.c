5a67f530e9d2233e81e75c59d6628e711c682d4f
Gichamu <gichamu007@gmail.com>
2025-08-02 23:49:59 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*_islower - checks for lowercase
+*@c: function parameter
+*Return: 1 if lowercase and 0 if otherwise
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
