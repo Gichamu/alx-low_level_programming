c4c04f2e8cbfa3247b690b2bf5ce9d986f4e2c2b
Gichamu <gichamu007@gmail.com>
2025-08-03 14:04:05 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_abs - prints absolute value of integer
+*@i: function parameter
+*Return: computes value of integer
+*/
+int _abs(int i)
+{
+	if (i > 0)
+	return (i);
+	else if (i == 0)
+	return (0);
+	else
+	return (-(i));
+}
