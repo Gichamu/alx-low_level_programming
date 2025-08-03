0e21154b8827c0d113cea80d51ef0224bfe9a16e
Gichamu <gichamu007@gmail.com>
2025-08-03 11:46:47 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_sign - prints sign of a number
+*@n: function parameter
+*Return: 1 if greater than zero, 0 if zero and - if less than 0
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (0);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar(0);
+	}
+		else
+		{
+			return (-1);
+			_putchar(-1);
+		}
+}
