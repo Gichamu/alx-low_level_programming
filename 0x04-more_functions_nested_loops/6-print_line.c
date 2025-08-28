e40a5920a8ff00e9efb290ca6ecae88c8c03a4a3
Gichamu <gichamu007@gmail.com>
2025-08-28 12:10:02 -0700

    ll commits not staged

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,18 +1,12 @@
 #include "main.h"
 
 /**
- * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
- *
- * Return: void
- */
+*print_line - drwsa a straight linr in terminal
+*@n: function parameter
+*Return: staight line
+*/
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		_putchar('_');
-	}
-	_putchar('\n');
+	if (n > 0)
+	_putchar(95);
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,10 +10,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int value = 0, i;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
+		{
 			value = s1[i] - s2[i];
+			break;
+		}
 	}
 	return (value);
 }
