981f9c157c5f7bb6910f36217dd285b614d50032
Gichamu <gichamu007@gmail.com>
2025-09-01 10:09:21 -0700

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,12 +8,12 @@
 char *cap_string(char *s)
 {
 	int i = 0, j = 0;
-	char spec[] = " \\t\\n,;.!?\"(){}";
+	char spec[] = " \t\n,;.!?\"(){}";
 
+	if (s[0]  >= 97 && s[0] <= 122)
+		s[0] = s[0] - 32;
 	while (s[i] != '\0')
 	{
-		if (s[0] >= 97 && s[0] <= 122)
-			s[0] = s[0] - 32;
 		for (j = 0; spec[j] != '\0'; j++)
 		{
 			if (s[i] == spec[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
